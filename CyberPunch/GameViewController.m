284d0fd265ed8c8d2180dd8a27fce0e6620462f2
Pedro Ruiz <pedro.ruiz.diaz@me.com>
2015-07-05 14:27:06 -0600

    Blank Canvas

--- CyberPunch/GameViewController.m
+++ CyberPunch/GameViewController.m
@@ -34,19 +34,6 @@
 {
     [super viewDidLoad];
 
-    // Configure the view.
-    SKView * skView = (SKView *)self.view;
-    skView.showsFPS = YES;
-    skView.showsNodeCount = YES;
-    /* Sprite Kit applies additional optimizations to improve rendering performance */
-    skView.ignoresSiblingOrder = YES;
-    
-    // Create and configure the scene.
-    GameScene *scene = [GameScene unarchiveFromFile:@"GameScene"];
-    scene.scaleMode = SKSceneScaleModeAspectFill;
-    
-    // Present the scene.
-    [skView presentScene:scene];
 }
 
 - (BOOL)shouldAutorotate
